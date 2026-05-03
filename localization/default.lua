92ed67269a8442b8a690b494b47487acd7e7f3d2
Mathematica-naneinf <mathematica2763@gmail.com>
2026-05-03 14:47:56 +0100

    Update descriptions
    Updated the descriptions in this file for the changes made to the Main File.

--- localization/default.lua
+++ localization/default.lua
@@ -17,7 +17,7 @@ return {
 			j_TLB_test = {
 				name = "Test Joker",
 				text = {
-					"Played {C:attention}unscoring{} cards give {C:purple}+#1#{} score.",
+					"Nil",
 					"{s:0.4} ",
 					"{s:0.8,C:inactive}(Effect will change between most updates, and",
 					"{s:0.8,C:inactive}may contain untested or broken code)",
@@ -67,6 +67,22 @@ return {
 					"Levels up the played hand, and",
 					"all hands contained within it",
 					"by {C:planet}#1#{} levels",
+				},
+			},
+			
+			j_TLB_rewrite = {
+				name = "Rewrite",
+				text = {
+					"Played {C:attention}unscoring{} cards",
+					"give {C:purple}+#1#{} score."
+				},
+			},
+			
+			j_TLB_hhaanndd = {
+				name = "Big Hands",
+				text = {
+					"{C:attention}+#1#{} hand size. Played, {C:attention}unscoring{} cards",
+					"trigger {C:attention}held in hand{} effects."
 				},
 			},
 
