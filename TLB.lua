b0284b4e5c30b14c7d3a7165bc25b89a4a99975c
Mathematica-naneinf <280715029+Mathematica-naneinf@users.noreply.github.com>
2026-08-19 17:09:11 +0100

    Load Editions.lua

--- TLB.lua
+++ TLB.lua
@@ -2,14 +2,20 @@
 -- Originally, this file contained all the code that makes everything work.
 -- Now, it instead just tells the game to load a bunch of other files that are what actually make things work.
 -- This change has been made for organisation reasons.
--- It also serves the secondary purpose of making debugging easier.
+-- It also serves the secondary purpose of making debugging slightly easier.
 
 
 
 
 
 assert(SMODS.load_file("Hooks.lua"))()
+
 assert(SMODS.load_file("Atlases.lua"))()
+
 assert(SMODS.load_file("Blinds.lua"))()
+
 assert(SMODS.load_file("Backs.lua"))()
+
+assert(SMODS.load_file("Editions.lua"))()
+
 assert(SMODS.load_file("Jokers.lua"))()
