fa0f8fd03a086efe2761de9215c17e5e2f3b6beb
Mathematica-naneinf <280715029+Mathematica-naneinf@users.noreply.github.com>
2026-08-19 17:09:35 +0100

    Add Editions.lua

---
+++ Editions.lua
@@ -0,0 +1,49 @@
+
+
+
+
+
+
+
+-- Frozen
+
+SMODS.Edition {
+	key = "frost",
+	shader = false,
+	unlocked = true,
+	discovered = true,
+	in_shop = false,
+	weight = 0,
+	extra_cost = -5,
+	apply_to_float = true,
+	never_scores = true
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
